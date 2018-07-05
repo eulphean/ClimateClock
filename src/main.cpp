b4a5338c6a0e06ab2149e0bc5ab67f45cefeec2c
Andrew McWilliams <andy@jahya.net>
2018-07-05 17:17:02 -0400

    Fixes protected member compiler error

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,14 @@ int main( ){
   ofGLFWWindowSettings settings;
   
   
-  settings.width = 400;
-  settings.height = 400;
+  settings.setSize(400, 400);
   settings.setPosition(ofVec2f(0,0));
   settings.resizable = true;
   settings.multiMonitorFullScreen = true;
   shared_ptr<ofAppBaseWindow> secondWindow = ofCreateWindow(settings);
   
   
-  settings.width = 600;
-  settings.height = 600;
+  settings.setSize(600, 600);
   settings.setPosition(ofVec2f(300,0));
   settings.resizable = true;
   settings.multiMonitorFullScreen = false;
