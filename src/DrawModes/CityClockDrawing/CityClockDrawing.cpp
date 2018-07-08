71032fcb0fe2d22c6911e3401a0351fd19af3ace
Andrew McWilliams <andy@jahya.net>
2018-07-08 12:54:13 -0400

    Draws three test clock parts for New York

--- src/DrawModes/CityClockDrawing/CityClockDrawing.cpp
+++ src/DrawModes/CityClockDrawing/CityClockDrawing.cpp
@@ -4,7 +4,7 @@ void CityClockDrawing::setup(ofxProjectionMask* projectionMask) {
     clock.setup();
     clockFace = projectionMask->newPattern(1080, 860);
     for(int i = 0; i < numPatterns; i++){
-        clockFaceParts.push_back(projectionMask->newPattern(400, 400));
+        clockFaceParts.push_back(projectionMask->newPattern(80, 35));
     }
 }
 
@@ -23,10 +23,30 @@ void CityClockDrawing::draw(string city) {
 void CityClockDrawing::drawNewYorkBuffers() {
     clockFace->begin();
     {
-        ofBackground(ofColor::white);
-        clock.drawClock();
+        ofClear(0, 0, 0, 0);
     }
     clockFace->end();
+    
+    clockFaceParts.at(0)->begin();
+    {
+        ofBackground(ofColor::green);
+        clock.drawDays(3, 30);
+    }
+    clockFaceParts.at(0)->end();
+
+    clockFaceParts.at(1)->begin();
+    {
+        ofBackground(ofColor::green);
+        clock.drawHrs(3, 30);
+    }
+    clockFaceParts.at(1)->end();
+
+    clockFaceParts.at(2)->begin();
+    {
+        ofBackground(ofColor::green);
+        clock.drawMins(3, 30);
+    }
+    clockFaceParts.at(2)->end();
 }
 
 void CityClockDrawing::drawTokyoBuffers() {
