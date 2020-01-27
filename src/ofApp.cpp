1bb06d19d8e10e52e31c8e0b364463f3a61f87ab
Amay Kataria <amkatari@microsoft.com>
2020-01-26 22:33:09 -0600

    Integrate color lerp for the background

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -4,15 +4,49 @@ void ofApp::setup() {
   centeredClock.setup();
   hideGui = true;
   bg.load("background.jpg");
+  
+  // Initialize colors.
+  colors.push_back(ofColor(211, 55, 35));
+  colors.push_back(ofColor(237, 177, 30));
+  colors.push_back(ofColor(22, 89, 144));
+  colors.push_back(ofColor(89, 179, 60));
+  colors.push_back(ofColor(220, 219, 218));
+  lerpAmt = 0.0; // Must go from 0-1.
+  curColIdx = 0;
+  
+  pressed = false;
 }
 
 void ofApp::update() {
   centeredClock.update();
+  handleColorLerp();
 }
 
 void ofApp::draw() {
-  bg.draw(0, 0, ofGetWidth(), ofGetWidth());
+  if (pressed) {
+    bg.draw(0, 0, ofGetWidth(), ofGetWidth());
+  } else {
+    ofBackground(lerped);
+  }
+  
   centeredClock.draw(hideGui);
+}
+
+void ofApp::handleColorLerp() {
+  // Color lerping
+  if (lerpAmt >= 1) {
+    // Swap colors
+    ofColor temp = end;
+    end = start;
+    start = temp;
+    curColIdx = (curColIdx + 1) % colors.size();
+    lerpAmt = 0;
+  }
+  
+  int nextColIdx = (curColIdx + 1) % colors.size();
+  start = colors[curColIdx]; end = colors[nextColIdx];
+  lerped = start.getLerped(end, lerpAmt);
+  lerpAmt = lerpAmt + 0.001;
 }
 
 void ofApp::exit(){
@@ -27,4 +61,9 @@ void ofApp::keyPressed(int key) {
 
 void ofApp::mousePressed(int x, int y, int button) {
   centeredClock.nextTimezone();
+  pressed = true;
+}
+
+void ofApp::mouseReleased(int x, int y, int button) {
+  pressed = false;
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -9,13 +9,23 @@ public:
     void setup();
     void update();
     void draw();
+    void handleColorLerp();
     void exit();
   
     void keyPressed(int key);
     void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button); 
   
 private:
     CenteredClock centeredClock;
     bool hideGui;
     ofImage bg;
+  
+    // Background color lerp
+    std::vector<ofColor> colors; ofColor start; ofColor end;
+    ofColor lerped;
+    float lerpAmt;
+    int curColIdx;
+  
+    bool pressed;
 };
