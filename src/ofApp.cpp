891e390d5cf0904945592a1a6acf1c33156e27df
Amay Kataria <amkatari@microsoft.com>
2020-01-26 22:07:23 -0600

    Remove creating things on touch interaction. Touch is not calibrated so the touch goes haywire

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -12,9 +12,6 @@ void ofApp::update() {
 
 void ofApp::draw() {
   bg.draw(0, 0, ofGetWidth(), ofGetWidth());
-  for (int i = 0; i < dips.size(); i++) {
-    dips[i].draw();
-  }
   centeredClock.draw(hideGui);
 }
 
@@ -30,15 +27,4 @@ void ofApp::keyPressed(int key) {
 
 void ofApp::mousePressed(int x, int y, int button) {
   centeredClock.nextTimezone();
-  
-  auto pos = glm::vec2(x, y);
-  cout << pos << endl; 
-  auto c = ofColor(ofRandom(255), ofRandom(255), ofRandom(255));
-  auto r = ofRandom(5, 10);
-  
-  Dip d (pos, c, r);
-  // dips.push_back(d);
 }
-
-// Animations (interpolate the background) = no
-// Touch response (animate/do something when something happens)
--- src/ofApp.h
+++ src/ofApp.h
@@ -3,29 +3,6 @@
 #include "ofMain.h"
 #include "CenteredClock.h"
 
-class Dip {
-  public:
-    Dip(glm::vec2 pos, ofColor col, int rad) {
-      position = pos;
-      color = col;
-      radius = rad;
-    }
-  
-    void draw() {
-      ofPushStyle();
-        ofPushMatrix();
-          ofSetColor(color);
-          ofTranslate(position);
-          ofDrawCircle(0, 0, radius);
-        ofPopMatrix();
-      ofPopStyle();
-    }
-  
-    glm::vec2 position;
-    ofColor color;
-    int radius;
-};
-
 class ofApp : public ofBaseApp{
 
 public:
@@ -41,5 +18,4 @@ private:
     CenteredClock centeredClock;
     bool hideGui;
     ofImage bg;
-    std::vector<Dip> dips; 
 };
